becec384ab5d31c084c73a123e6dea80a268e7a5
Sumangotame <iamsuperstarsuman@gmail.com>
2024-04-18 20:56:38 +0545

    modified auth controller for code cleanup

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,10 +2,8 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :authorize_request
   def create
-    email = params.dig(:auth, :email)
-    password = params.dig(:auth, :password)
-    user=User.find_by(email:email)
-    if user && user.authenticate(password)
+    user=User.find_by(email:sign_in_params[:email])
+    if user && user.authenticate(sign_in_params[:password])
       token=generate_token(user)
       render json: {"token":token}
     else
@@ -14,6 +12,10 @@ class Api::V1::AuthController < ApplicationController
   end
 
   private
+
+  def sign_in_params
+    params.require(:auth).permit(:email,:password)
+  end
 
   def generate_token(user)
     JWT.encode({user_id:user.id},Rails.application.secret_key_base)
